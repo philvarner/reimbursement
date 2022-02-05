8f288493046ddb78ce0ff880a23b07291698b0e8
Phil Varner <philvarner@gmail.com>
2022-02-04 22:43:11 -0500

    working implementation

---
+++ main.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from datetime import datetime, timedelta
+
+from reimburse import ProjectDays, State, Day
+
+
+def main():
+
+    # Set 1:
+    #  Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15
+
+    s1_p1 = ProjectDays.from_range("2015-09-01", "2015-09-01", State.WORK_LOW)
+
+    # Set 2:
+    #   Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15
+    #   Project 2: High Cost City Start Date: 9/2/15 End Date: 9/6/15
+    #   Project 3: Low Cost City Start Date: 9/6/15 End Date: 9/8/15
+
+    s2_p1 = ProjectDays.from_range("2015-09-01", "2015-09-01", State.WORK_LOW)
+    s2_p2 = ProjectDays.from_range("2015-09-02", "2015-09-06", State.WORK_HIGH)
+    s2_p3 = ProjectDays.from_range("2015-09-06", "2015-09-08", State.WORK_LOW)
+
+    print((s2_p1 + s2_p2).reimbursement())
+
+    # Set 3:
+    #   Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15
+    #   Project 2: High Cost City Start Date: 9/5/15 End Date: 9/7/15
+    #   Project 3: High Cost City Start Date: 9/8/15 End Date: 9/8/15
+
+    s3_p1 = ProjectDays.from_range("2015-09-01", "2015-09-03", State.WORK_LOW)
+    s3_p2 = ProjectDays.from_range("2015-09-05", "2015-09-07", State.WORK_HIGH)
+    s3_p3 = ProjectDays.from_range("2015-09-08", "2015-09-08", State.WORK_HIGH)
+
+    # Set 4:
+    #   Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15
+    #   Project 2: Low Cost City Start Date: 9/1/15 End Date: 9/1/15
+    #   Project 3: High Cost City Start Date: 9/2/15 End Date: 9/2/15
+    #   Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15
+
+    s4_p1 = ProjectDays.from_range("2015-09-01", "2015-09-01", State.WORK_LOW)
+    s4_p2 = ProjectDays.from_range("2015-09-01", "2015-09-01", State.WORK_LOW)
+    s4_p3 = ProjectDays.from_range("2015-09-02", "2015-09-02", State.WORK_HIGH)
+    s4_p4 = ProjectDays.from_range("2015-09-02", "2015-09-03", State.WORK_HIGH)
+
+
+if __name__ == "__main__":
+    main()
--- reimburse/__init__.py
+++ reimburse/__init__.py
@@ -1,69 +1,121 @@
-
 from __future__ import annotations
 from enum import Enum
 from datetime import datetime, timedelta
-from re import I
 from typing import List
 from dataclasses import dataclass
+from functools import reduce
+import copy
+
 
 class State(Enum):
-  OPEN = 1
-  TRAVEL_LOW = 2
-  TRAVEL_HIGH = 3
-  WORK_LOW = 4
-  WORK_HIGH = 5
+    OPEN = 1
+    TRAVEL_LOW = 2
+    TRAVEL_HIGH = 3
+    WORK_LOW = 4
+    WORK_HIGH = 5
 
-  def __lt__(self, other):
-     if self.__class__ is other.__class__:
-       return self.value < other.value
-     return NotImplemented
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
 
-  def combine(self, state2: State) -> State:
-    return max(self, state2)
+    def __add__(self, state2: State) -> State:
+        return max(self, state2)
+
+    def reimbursement(self) -> int:
+        if self == self.TRAVEL_LOW:
+            return 45
+        elif self == self.TRAVEL_HIGH:
+            return 55
+        elif self == self.WORK_LOW:
+            return 75
+        elif self == self.WORK_HIGH:
+            return 85
+        else:
+            return 0
+
 
 @dataclass
 class Day:
 
-  date: datetime.date
-  state: State
+    date: datetime.date
+    state: State
 
-  def __eq__(self, other):
-    if self.__class__ is other.__class__:
-      return self.date == other.date
-    return NotImplemented
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self.date == other.date
+        return NotImplemented
 
-  def __lt__(self, other):
-    if self.__class__ is other.__class__:
-      return self.date < other.date
-    return NotImplemented
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.date < other.date
+        return NotImplemented
 
-  def combine(self, other: Day) -> Day:
-    if self.date != other.date:
-      raise Exception("tried to combine Days that had different dates")
-    return Day(self.date, self.state.combine(other.state))
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.date > other.date
+        return NotImplemented
+
+    def __add__(self, other: Day) -> Day:
+        if self.date != other.date:
+            raise Exception(
+                f"tried to combine Days that had different dates: {self.date} {other.date}"
+            )
+        return Day(self.date, self.state + other.state)
+
 
 class ProjectDays:
+    def __init__(self, days: List[Day]) -> ProjectDays:
+        self.days = days
 
-  def __init__(self, start: str, end: str, state: State) -> None:
+    @classmethod
+    def from_range(cls, start: str, end: str, state: State) -> None:
 
-    start_date = datetime.fromisoformat(start)
-    end_date = datetime.fromisoformat(end)
+        start_date = datetime.fromisoformat(start)
+        end_date = datetime.fromisoformat(end)
 
-    if start_date > end_date:
-      raise Exception("start date not before end date")
+        if start_date > end_date:
+            raise Exception("start date not before end date")
 
-    pre_date = start_date - timedelta(days=1)
-    day_count = (end_date - start_date).days + 3 # 3 = before + after + end inclusive
-    self.days: List[Day] = \
-      [Day(pre_date + timedelta(days=x), state) for x in range(day_count)]
-    self.days[0].state = State.OPEN
-    self.days[-1].state = State.OPEN
+        pre_date = start_date - timedelta(days=1)
+        day_count = (
+            end_date - start_date
+        ).days + 3  # 3 = before + after + end inclusive
+        pd = ProjectDays(
+            [Day(pre_date + timedelta(days=x), state) for x in range(day_count)]
+        )
+        pd.days[0].state = State.OPEN
+        pd.days[-1].state = State.OPEN
 
-  def __str__(self):
-     return str(self.days)
+        return pd
 
-  def combine(self, other: ProjectDays) -> ProjectDays:
-    pass
+    def __str__(self):
+        return str(self.days)
 
-  def populate_travel_days(self) -> ProjectDays:
-    pass
+    def __add__(self, other: ProjectDays) -> ProjectDays:
+        all_days = sorted(self.days + other.days)
+
+        def f(xs, y):
+            if xs[-1] == y:
+                xs[-1] == xs[-1] + y
+            else:
+                xs.append(y)
+            return xs
+
+        return ProjectDays(reduce(f, all_days, all_days[0:1]))
+
+    def _populate_travel_days(self) -> ProjectDays:
+        new_days = copy.deepcopy(self.days)
+        for i, day in enumerate(new_days[1:-1], start=1):
+            if (
+                new_days[i - 1].state == State.OPEN
+                or new_days[i + 1].state == State.OPEN
+            ):
+                if day.state == State.WORK_HIGH:
+                    day.state = State.TRAVEL_HIGH
+                elif day.state == State.WORK_LOW:
+                    day.state = State.TRAVEL_LOW
+        return new_days
+
+    def reimbursement(self):
+        return sum([d.state.reimbursement() for d in self._populate_travel_days()])
